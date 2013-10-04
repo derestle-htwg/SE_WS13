81f68b9b24ec9783affaa60acb2b95f71d3e2733
derestle <derestle@htwg-konstanz.de>
2013-10-04 17:22:46 +0200

    testbranch3

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,8 +4,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0;
-		System.out.println("testBranch5");
-		i++;
+		System.out.println("testBranch6");
+		i+=2;
 		System.out.println(i);
 	}
 
