85c2050a3887bac91f2edea9d3d13f32aaceaa9e
derestle <derestle@htwg-konstanz.de>
2013-10-04 17:35:22 +0200

    CommitAfterMerge

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0;
-		System.out.println("master_branchmerged_7");
+		System.out.println("master_branchmerged__Next2");
 		i+=2;
 		System.out.println(i);
 	}
